3c8423f02c15a288cd615e14fb89e042e99f236a
Zeynep Merve Koç <25181617005@gazi.edu.tr>
2025-12-24 07:55:04 +0300

    Kod güncellendi

--- 2-5-proje.c
+++ 2-5-proje.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Ogrenci
+// Öğrenci bilgilerini tutan yapı
+struct Ogrenci 
 {
     char ad[30];
     char soyad[30];
@@ -12,9 +13,9 @@ struct Ogrenci
 };
 
 int main(){
-    struct Ogrenci *ogr = NULL; 
-    struct Ogrenci gecici;
-    int n = 0;
+    struct Ogrenci *ogr = NULL; // Dinamik öğrenci dizisi (başlangıçta boş)
+    struct Ogrenci gecici; // Dosyadan okuma için geçici değişken
+    int n = 0; // Okunan öğrenci sayısı
     
     FILE *dosya1 = fopen("ogrenci.txt", "r");
     if (dosya1 == NULL)
@@ -31,6 +32,7 @@ int main(){
         return 1;
     }
 
+    // Dosyadan öğrenci bilgileri satır satır okunur. EOF =  End Of File
     while (fscanf(dosya1, "%s %s %f %f %f",
          gecici.ad, 
          gecici.soyad, 
@@ -40,11 +42,13 @@ int main(){
     {
         if (n == 0)
         {
+            // İlk öğrenci için malloc ile bellek ayrılır.
             ogr = (struct Ogrenci *)malloc(sizeof(struct Ogrenci)); 
         }
 
         else
         {
+            // Sonraki öğrenciler için realloc ile bellek genişletilir.
             ogr = (struct Ogrenci *)realloc(ogr, (n+1) * sizeof(struct Ogrenci));
         }
 
@@ -56,8 +60,8 @@ int main(){
             return 1;
         }
 
-        ogr[n] = gecici;
-        n++;
+        ogr[n] = gecici; // Geçici değişkende tutulan öğrenci diziye aktarılır.
+        n++; //Öğrenci sayısı artırılır.
     }
 
     if (n == 0)
@@ -70,16 +74,18 @@ int main(){
 
     for (int i = 0; i < n; i++)
     {
+        // Her öğrenci için toplam puan hesaplanır.
         ogr[i].toplam = (ogr[i].ort * 0.25) + (ogr[i].ales * 0.50) + (ogr[i].uds * 0.25);
     }
 
-    for (int i = 0; i < (n - 1); i++)
+    // Öğrenciler toplam puana göre büyükten küçüğe sıralanır.
+    for (int i = 0; i < (n - 1); i++) // i: Her adımda sırası kesinleşen öğrencinin dizideki indeksini tutar.
     {
-        for (int j = i + 1; j < n; j++)
+        for (int j = i + 1; j < n; j++)  // j: i indeksindeki öğrenciyi, kendisinden sonraki tüm öğrencilerle karşılaştırır.
         {
-            if (ogr[j].toplam > ogr[i].toplam)
+            if (ogr[j].toplam > ogr[i].toplam) // Eğer j indeksindeki öğrencinin toplam puanı i indeksindeki puandan daha büyükse yer değiştirirler
             {
-                struct Ogrenci temp;
+                struct Ogrenci temp;  // Öğrencilerin yerini değiştirmek için geçici değişken
                 temp = ogr[i];
                 ogr[i] = ogr[j];
                 ogr[j] = temp;
@@ -87,9 +93,11 @@ int main(){
         }
     }
    
+    // Dosyaya başlık satırı yazılır.
      fprintf(dosya2, "%-10s %-10s %-10s %-10s %-10s %-10s\n",
         "Ad", "Soyad", "Ortalama", "ALES", "UDS", "Toplam");
 
+    // En yüksek puanlı ilk 5 öğrenci dosyaya yazılır.
     for (int i = 0; i < 5; i++)
     {
         fprintf(dosya2, "%-10s %-10s %-10.2f %-10.2f %-10.2f %-10.2f\n", 
@@ -101,7 +109,9 @@ int main(){
         ogr[i].toplam);
     }
 
-    free(ogr);
+    free(ogr);  // Ayrılan bellek serbest bırakılır.
+
+    //Dosyalar kapatılır.
     fclose(dosya1);
     fclose(dosya2);
     
